3a416483aedd5984b201e081f55ce8de3fc8c33b
Emiliano Guerrero <12738378+mrtotopo@users.noreply.github.com>
2026-06-22 01:45:35 -0600

    feat: add autocompletion for commands, files and dirs

--- src/pyre/main.py
+++ src/pyre/main.py
@@ -3,27 +3,30 @@ import os
 import readline
 import socket
 
-from pyre.utils.autocompletions import path_completer
+from pyre.utils.autocompletions import smart_completer
 
 
 def main():
+    readline.set_completer(smart_completer)  # Set the custom completer function for autocompletion
+    readline.parse_and_bind('tab: complete')  # Set Tab key to trigger autocompletion
+
     while True:
         current_path = os.getcwd()
-        home_dir = os.path.expanduser("~")
+        home_dir = os.path.expanduser("~")  # Get the home directory path
 
         if current_path.startswith(home_dir):
-            current_path = current_path.replace(home_dir, "~", 1)
+            current_path = current_path.replace(home_dir, "~", 1)  # Replace the home directory with ~
 
         username = getpass.getuser()
         hostname = socket.gethostname()
 
-        user_input = input(f"{username}@{hostname}:{current_path} $ ")
+        user_input = input(f"{username}@{hostname}:{current_path} $ ")  # Wait the user input
 
         if not user_input:
             continue
 
-        args = user_input.split()
-        command = args[0]
+        args = user_input.split()  # Get arguments from the user input
+        command = args[0]  # Get the command
 
         if command == "exit":
             break
@@ -31,20 +34,20 @@ def main():
         if command == "cd":
             try:
                 if len(args) == 1:
-                    os.chdir(os.path.expanduser("~"))
+                    os.chdir(os.path.expanduser("~"))  # If no argument is provided, change to the home directory
                 else:
-                    os.chdir(args[1])
+                    os.chdir(args[1])  # Move to the specified directory
             except FileNotFoundError:
                 print(f"pyre: cd: {args[1]}: No such file or directory")
             except NotADirectoryError:
                 print(f"pyre: cd: {args[1]}: Not a directory")
             continue
 
-        pid = os.fork()
+        pid = os.fork()  # Create a child process to execute the command
 
         if pid == 0:  # Child process
             try:
-                os.execvp(command, args)
+                os.execvp(command, args)  # Replace the current process with the new command, passing the arguments
             except FileNotFoundError:
                 print(f"pyre: {command}: command not found")
             except Exception as e:
@@ -56,7 +59,4 @@ def main():
 
 
 if __name__ == '__main__':
-    readline.set_completer(path_completer)
-    readline.parse_and_bind('tab: complete')
-
     main()
--- src/pyre/utils/autocompletions.py
+++ src/pyre/utils/autocompletions.py
@@ -1,11 +1,44 @@
 import glob
+import os
+
+import readline
 
 
-def path_completer(text: str, state: int) -> str | None:
+def get_system_commands(prefix: str) -> list[str]:
     """
-    Simple path completer function for readline
+    Search directories in the system PATH for executables that start with the given prefix.
     """
-    matches = glob.glob(text + '*')
+    commands: set = set()
+    path_env: str = os.environ.get("PATH", "") # Get PATH environment variable
+    directories: list[str] = path_env.split(os.pathsep) # Get directories in PATH
+
+    for directory in directories:
+        if os.path.isdir(directory): # Check if is a directory
+            try:
+                for file in os.listdir(directory): # Get files in the directory
+                    if file.startswith(prefix):
+                        full_path: str = os.path.join(directory, file) # Get the fullpath of the file
+                        if os.access(full_path, os.X_OK): # If the file has execute permissions
+                            commands.add(file) # It's added to the list of commands
+            except PermissionError:
+                continue
+
+    return list(commands)
+
+
+def smart_completer(text: str, state: int) -> str | None:
+    """
+    A custom completer function that provides autocompletion for commands and paths based on the current input.
+    """
+    buffer: str = readline.get_line_buffer() # Get written text in the current line
+
+    if " " not in buffer.lstrip():
+        matches: list[str] = get_system_commands(text)  # Search for commands in PATH
+        matches.extend(glob.glob(text + '*'))  # Add local files
+        matches: list[str] = sorted(list(set(matches)))  # Remove duplicates and sort
+    else:
+        matches: list[str] = glob.glob(text + '*')  # Search local files or directories
+
     if state < len(matches):
         return matches[state]
     else:
